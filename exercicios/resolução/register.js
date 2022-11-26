cfd29d2d2c09b3ad3b1d02e5242f5abf58ed2d6c
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2022-11-26 10:58:40 -0300

    update register

--- exercicios/resolução/register.js
+++ exercicios/resolução/register.js
@@ -13,6 +13,6 @@ const register = {
 // const date = new Date();
 // register.initialTime = date.getHours()+":"+date.getMinutes()+":"+ date.getSeconds();
 
-console.log(register);
+console.log(`Registro do jogo: ${register}`);
 
 module.exports = register;
