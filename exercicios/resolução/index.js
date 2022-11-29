dff55852685c44454e4029ec7efd50c3c5e1cbd1
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2022-11-29 19:17:26 -0300

    improve code

--- exercicios/resolução/index.js
+++ exercicios/resolução/index.js
@@ -1,1 +1,18 @@
-//exercicios feitos pela profa aqui
+const { Survivor, SurvivorLevel, SurvivorEquipment } = require('./survivor');
+
+const sobrevivente1 = new Survivor('Luara');
+// sobrevivente1.points = 6;
+// const nivel_sobrevivente1 = new SurvivorLevel(sobrevivente1);
+// nivel_sobrevivente1.checkLevel();
+// sobrevivente1.points = 20;
+// nivel_sobrevivente1.checkLevel();
+// sobrevivente1.points = 45;
+// nivel_sobrevivente1.checkLevel();
+
+const equipamento1 = new Equipment('agua', 'utilitarios');
+const equipamentos_sobrevivente1 = new SurvivorEquipment(sobrevivente1);
+equipamentos_sobrevivente1.addEquipment(equipamento1);
+equipamentos_sobrevivente1.removeEquipment(equipamento1);
+
+console.log(register);
+// console.log(sobrevivente);
--- exercicios/resolução/survivor.js
+++ exercicios/resolução/survivor.js
@@ -1,13 +1,12 @@
-const { FormatDate } = require('./helpers');
-const register = require('./register');
+const { registerMessage } = require('./helpers');
 const Level = require('./levels');
 const GameCharacter = require('./character');
 const Equipment = require('./equipment');
 
 class Survivor extends GameCharacter {
 	name;
-	handsEquipments;
-	bagEquipments;
+	handsEquipments = [];
+	bagEquipments = [];
 	points = 0;
 	level = Level.Azul;
 
@@ -18,11 +17,7 @@ class Survivor extends GameCharacter {
 		this.MAX_OF_HURTS = 3;
 		this.TOTAL_OF_ACTIONS = 3;
 
-		this.date = FormatDate();
-		//Criar função no helpers pra registrar
-		register.survivors.push(
-			`${this.date}: Um sobrevivente de nome ${name} foi criado`
-		);
+		registerMessage('survivors', `Um sobrevivente de nome ${name} foi criado`);
 	}
 }
 
@@ -51,10 +46,9 @@ class SurvivorLevel {
 
 		const updatedLevel = this.survivor.level;
 		if (currentLevel !== updatedLevel) {
-			const formattedDate = FormatDate();
-
-			register.survivorsLevels.push(
-				`${formattedDate}: O sobrevivente de nome ${this.survivor.name} subiu para o nível ${this.survivor.level}`
+			registerMessage(
+				'survivorsLevels',
+				`O sobrevivente de nome ${this.survivor.name} subiu para o nível ${this.survivor.level}`
 			);
 		}
 	}
@@ -84,21 +78,20 @@ class SurvivorEquipment {
 	addEquipment(equipment) {
 		if (equipment instanceof Equipment) {
 			if (this.handsSlot < this.MAX_OF_HANDS_SLOTS) {
-				// Guardar na mão
 				this.survivor.handsEquipments.push(equipment);
 				this.handsSlot++;
-				register.survivorsEquipments.push(
-					`${formattedDate}: O sobrevivente de nome ${this.survivor.name} adicionou o equipamento ${equipment} no slot das mãos.`
+				registerMessage(
+					'survivorsEquipments',
+					`O sobrevivente de nome ${this.survivor.name} adicionou o equipamento ${equipment.name} no slot das mãos.`
 				);
 			} else if (this.bagSlot < this.MAX_OF_BAG_SLOTS) {
-				// Guardar na bag
 				this.survivor.bagEquipments.push(equipment);
 				this.bagSlot++;
-				register.survivorsEquipments.push(
-					`${formattedDate}: O sobrevivente de nome ${this.survivor.name} adicionou o equipamento ${equipment} no slot das reservas.`
+				registerMessage(
+					'survivorsEquipments',
+					`O sobrevivente de nome ${this.survivor.name} adicionou o equipamento ${equipment.name} no slot das reservas.`
 				);
 			} else {
-				// Informar que precisa descartar alguma arma
 				console.log(
 					'Você está com seus slots cheios. Escolha um equipamento para remover antes de adicionar um novo.'
 				);
@@ -109,6 +102,9 @@ class SurvivorEquipment {
 
 	removeEquipment(equipmentToRemove) {
 		if (equipmentToRemove instanceof Equipment) {
+			const qteHandsEquipments = this.survivor.handsEquipments.length;
+			const qteBagEquipments = this.survivor.bagEquipments.length;
+
 			this.survivor.handsEquipments = this.survivor.handsEquipments.filter(
 				(equipment) => equipment !== equipmentToRemove
 			);
@@ -117,26 +113,17 @@ class SurvivorEquipment {
 				(equipment) => equipment !== equipmentToRemove
 			);
 
-			//Só pode registrar se o equipamento realmente foi removido
-			register.survivorsEquipments.push(
-				`${formattedDate}: O sobrevivente de nome ${this.survivor.name} removeu o equipamento ${equipmentToRemove}.`
-			);
+			if (
+				qteHandsEquipments !== this.survivor.handsEquipments.length ||
+				qteBagEquipments !== this.survivor.bagEquipments.length
+			) {
+				registerMessage(
+					'survivorsEquipments',
+					`O sobrevivente de nome ${this.survivor.name} removeu o equipamento ${equipmentToRemove}.`
+				);
+			}
 		}
 	}
 }
 
-// Instanciar tudo em um único arquivo (index ou main, por exemplo)
-const sobrevivente1 = new Survivor('Luara');
-// sobrevivente1.points = 6;
-// const nivel_sobrevivente1 = new SurvivorLevel(sobrevivente1);
-// nivel_sobrevivente1.checkLevel();
-// sobrevivente1.points = 20;
-// nivel_sobrevivente1.checkLevel();
-// sobrevivente1.points = 45;
-// nivel_sobrevivente1.checkLevel();
-
-// const equipamentos_sobrevivente1 = new SurvivorEquipment(sobrevivente1);
-// equipamentos_sobrevivente1.addEquipment();
-
-console.log(register);
-// console.log(sobrevivente);
+module.exports = { Survivor, SurvivorLevel, SurvivorEquipment };
