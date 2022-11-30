fb24284849b95a08662e229f02972da1dad18a9c
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2022-11-29 21:37:41 -0300

    create actions class

--- exercicios/resolução/survivor.js
+++ exercicios/resolução/survivor.js
@@ -194,10 +194,29 @@ class SurvivorHurts extends SurvivorUtils {
 	}
 }
 
+class SurvivorActions extends SurvivorUtils {
+	constructor(survivor) {
+		super(survivor);
+	}
+
+	doAction() {
+		if (this.survivor.actions < this.survivor.TOTAL_OF_ACTIONS) {
+			this.survivor.actions++;
+			registerMessage(
+				'actions',
+				`O sobrevivente de nome ${this.survivor.name} realizou uma ação`
+			);
+		} else {
+			console.log('Esse sobrevivente já realizou o máximo de ações na rodada.');
+		}
+	}
+}
+
 module.exports = {
 	Survivor,
 	SurvivorLevel,
 	SurvivorEquipment,
 	SurvivorAbilitiesTree,
 	SurvivorHurts,
+	SurvivorActions,
 };
