b381de8a7c0dfda0600ecf332aa688c70d0afec8
maparecidasousa <aparecidasousanunes@gmail.com>
2022-12-01 20:34:14 -0300

    ”comentário”

--- exercicios/resolução/index.js
+++ exercicios/resolução/index.js
@@ -1,1 +1,75 @@
-//exercicios feitos pela profa aqui
+const {
+  Survivor,
+  SurvivorLevel,
+  SurvivorEquipment,
+  SurvivorAbilitiesTree,
+  SurvivorPoints,
+  SurvivorHurts,
+  SurvivorActions,
+} = require("./survivor");
+const AbilitiesTree = require("./abilitiesTree");
+const Equipment = require("./equipment");
+const register = require("./register");
+
+const abilitiesLevelYellow1 = ["+1 haduke"];
+const abilitiesLevelOrange1 = ["+1 ação", "tesouro escondido"];
+const abilitiesLevelRed1 = ["+1 vida", "+1 equipamento em mãos", "+1 dano"];
+
+const sobrevivente1 = new Survivor("Luara");
+const arvoreDeHabilidades1 = new AbilitiesTree(
+  abilitiesLevelYellow1,
+  abilitiesLevelOrange1,
+  abilitiesLevelRed1
+);
+
+const sobrevivente_arvore = new SurvivorAbilitiesTree(
+  sobrevivente1,
+  arvoreDeHabilidades1
+);
+
+sobrevivente1.points = 5;
+const nível_sobrevivente1 = new SurvivorLevel(sobrevivente1);
+nível_sobrevivente1.checkLevel();
+sobrevivente_arvore.unlockAbilities();
+
+const points_Sobrevivente1 = new SurvivorPoints(sobrevivente1);
+points_Sobrevivente1.killZombie();
+nível_sobrevivente1.checkLevel();
+sobrevivente_arvore.unlockAbilities();
+
+sobrevivente1.points = 20;
+nível_sobrevivente1.checkLevel();
+sobrevivente_arvore.unlockAbilities();
+
+sobrevivente1.points = 45;
+nível_sobrevivente1.checkLevel();
+sobrevivente_arvore.unlockAbilities();
+
+sobrevivente1.points = 61;
+nível_sobrevivente1.checkLevel();
+sobrevivente_arvore.unlockAbilities();
+
+sobrevivente1.points = 86;
+nível_sobrevivente1.checkLevel();
+sobrevivente_arvore.unlockAbilities();
+
+sobrevivente1.points = 150;
+nível_sobrevivente1.checkLevel();
+sobrevivente_arvore.unlockAbilities();
+
+const equipamento1 = new Equipment("agua", "utilitarios");
+const equipamentos_sobrevivente1 = new SurvivorEquipment(sobrevivente1);
+equipamentos_sobrevivente1.addEquipment(equipamento1);
+equipamentos_sobrevivente1.removeEquipment(equipamento1);
+
+const sobrevivente_ferimento1 = new SurvivorHurts(sobrevivente1);
+sobrevivente_ferimento1.getHurt();
+sobrevivente_ferimento1.getHurt();
+
+const action1 = new SurvivorActions(sobrevivente1);
+action1.doAction();
+action1.doAction();
+action1.doAction();
+action1.doAction();
+
+console.log(register);
---
+++ exercicios/resolução/register.js
@@ -0,0 +1,15 @@
+const register = {
+  initialTime: "",
+  survivors: [],
+  equipment: [],
+  hurts: [],
+  dies: [],
+  abilities: [],
+  survivorsEquipments: [],
+  survivorsLevels: [],
+  actions: [],
+  gameLevel: [],
+  finalTime: "",
+};
+
+module.exports = register;
