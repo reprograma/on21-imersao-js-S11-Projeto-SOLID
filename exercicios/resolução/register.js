3612005ad57b99d935769bf980e126dce5863a23
Mariana Trombini <mariana.trombini@dasa.com.br>
2022-12-01 11:00:20 -0300

    :rocket: feat: add register for game logs

--- exercicios/resolução/register.js
+++ exercicios/resolução/register.js
@@ -12,10 +12,7 @@ const register = {
 	finalTime: '',
 };
 
-// const date = new Date();
-// register.initialTime = date.getHours()+":"+date.getMinutes()+":"+ date.getSeconds();
-
-// console.log(`Registro do jogo: ${register}`);
-// console.log(register);
+const date = new Date();
+register.initialTime = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 
 module.exports = register;
