787edb3ed86adf23730e623c37d45dd85e842bf5
Eliane Almeida <elianevia@gmail.com>
2022-12-02 11:57:26 -0300

    Criando class hurts e removerEquipamento

---
+++ exercicios/entregas/eliane-almeida/propriedades/classHurts.js
@@ -0,0 +1,37 @@
+import { registerMessage } from "./helpers.js";
+
+export class Hurts{
+    character;
+    typecharacter;
+    constructor(character , typecharacter){
+        this.character = character;
+        this.typecharacter = typecharacter;
+    }
+        getHurt(){
+        if (this.character.alive) {
+
+        this.character.hurts++;
+        
+        registerMessage(
+            'hurts',
+            `The ${this.typecharacter} ${this.character.name} got hurt.`
+        );
+            if(this.typecharacter === 'survivors'){
+
+            const auxqtdequipment = this.character.handsEquipments.length-1;
+            const remove_equipamentos = new RemoveEquipment(this.character.handsEquipments[auxqtdequipment], this.character); 
+
+            remove_equipamentos.removeEquipment(this.character.handsEquipments[auxqtdequipment]);
+        }
+
+        if (this.character.hurts >= this.character.MAX_OF_HURTS) 
+            {
+                this.character.alive = false;
+                registerMessage(
+                    'dies',
+                    `The ${this.typecharacter} ${this.character.name} went from that for the better.`
+                );  
+            }
+        }
+    }
+}
