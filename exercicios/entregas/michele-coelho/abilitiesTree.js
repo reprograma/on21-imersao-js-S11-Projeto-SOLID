c7a5ccabc54e83cbcb74c49b92b7b5d01a385f9d
Michele Coelho <michelleset@hotmail.com>
2022-12-02 00:05:55 -0300

    Atividade semana 11

---
+++ exercicios/entregas/michele-coelho/abilitiesTree.js
@@ -0,0 +1,12 @@
+class AbilitiesTree {
+	abilitiesLevelYellow = [];
+	abilitiesLevelOrange = [];
+	abilitiesLevelRed = [];
+
+	constructor(abilitiesLevelYellow, abilitiesLevelOrange, abilitiesLevelRed) {
+		this.abilitiesLevelYellow = abilitiesLevelYellow;
+		this.abilitiesLevelOrange = abilitiesLevelOrange;
+		this.abilitiesLevelRed = abilitiesLevelRed;
+	}
+}
+module.exports = AbilitiesTree;
