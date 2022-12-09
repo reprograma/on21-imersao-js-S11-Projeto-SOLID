7d1a3b01b8ef844adc428f1993ee34d5d4df7188
Silvovisk <vascsilvia@gmail.com>
2022-12-09 11:36:16 -0300

    exercicio s10

---
+++ exercicios/entregas/silvia-vasconcelos/zombie.js
@@ -0,0 +1,31 @@
+const GameCharacter = require('./character');
+const { registerMessage } = require('./helpers');
+
+
+class Zombie extends GameCharacter{
+    name;
+ constructor(name) {
+    super();
+    this.name = name;
+    this.MAX_OF_HURTS = 2;
+    this.TOTAL_OF_ACTIONS =1;
+
+
+}
+}
+class ZombieUtils  extends Zombie{
+	zombie;
+
+	constructor(zombie) {
+        super();
+		if (zombie instanceof Zombie) {
+			this.zombie = zombie;
+		}
+        registerMessage('zombie', `the name zombie ${zombie.name} was raised.`);
+
+    }
+
+
+}
+
+module.exports ={ Zombie ,ZombieUtils};
